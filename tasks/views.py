cbca15c70e11d2c010cff6f2304f5ca6039e29a4
Emaus025 <remiileyva@gmail.com>
2025-02-13 20:07:10 -0600

    Agregado de serializers

--- tasks/views.py
+++ tasks/views.py
@@ -8,11 +8,20 @@ from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
 
 def signup(request):
 
